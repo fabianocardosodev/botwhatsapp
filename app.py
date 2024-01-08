9fbe2f055d35db03a7b014969626ef185e2a53ab
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2024-01-08 00:06:30 -0300

    ,

---
+++ app.py
@@ -0,0 +1,14 @@
+""" 
+PRECISO AUTOMATIZAR MINHAS MENSAGENS P/ MEUS CLIENTES MANDAR MENSGS DE COBRANÇA
+"""
+
+#Descrever os passos manuais e dps transformar em código
+# Ler planilha e guardar informações, nome, telefone e data vencimento
+#criar links personalidos do whatsaap e enviar msgs
+
+import openpyxl
+
+workbook = openpyxl.load_workbook('listacontato.xlsx')
+pagina_clientes = workbook['Sheet1']
+
+for linha in paginas_clientes.iter_rows(cace")
